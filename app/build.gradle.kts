b58d8133d3325fe1d11a71d55fcd9a86f6a3241b
Q <170919602+202420505@users.noreply.github.com>
2025-05-22 16:10:12 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,5 +39,5 @@ java {
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("io.appwrite:sdk-for-android:7.0.0")
+    implementation("io.appwrite:sdk-for-android:8.1.0")
 }

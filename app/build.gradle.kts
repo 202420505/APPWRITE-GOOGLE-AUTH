b4c2f311e3d9b09bdfe96fc97f853a16c6504a46
Q <170919602+202420505@users.noreply.github.com>
2025-06-08 17:54:38 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,11 +6,11 @@ plugins {
 }
 
 android {
-    namespace = "me.moontree.test.appwrite.auth"
+    namespace = "me.moontree.test.appwrite.auth.app1"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "me.moontree.test.appwrite.auth"
+        applicationId = "me.moontree.test.appwrite.auth.app1"
         minSdk = 21
         targetSdk = 34
         versionCode = 1
